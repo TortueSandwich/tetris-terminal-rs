a8ddcc39571ee489fa3329543bbad801a899965f
Arthur SAILLANT <artsaillant@gmail.com>
2023-06-23 20:35:17 +0200

    made holding setion clean

--- src/erreur.rs
+++ src/erreur.rs
@@ -9,3 +9,25 @@ impl fmt::Display for InvalidCoordinatesError {
     }
 }
 impl std::error::Error for InvalidCoordinatesError {}
+
+#[derive(Debug)]
+pub struct AlreadyHolding;
+
+impl fmt::Display for AlreadyHolding {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Hold is already holding a tetro")
+    }
+}
+
+impl std::error::Error for AlreadyHolding {}
+
+#[derive(Debug)]
+pub struct NothingHolded;
+
+impl fmt::Display for NothingHolded {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        write!(f, "Hold is not holding a tetro")
+    }
+}
+
+impl std::error::Error for NothingHolded {}
--- src/systems/hold/data.rs
+++ src/systems/hold/data.rs
@@ -1,25 +1,47 @@
-use crate::utils::{tetromino::Tetromino, container::*};
+use crate::{utils::{tetromino::Tetromino, container::*}, erreur::{AlreadyHolding, NothingHolded}};
+use std::mem;
 
 pub struct Hold {
-    pub holded_polyomino: Option<Tetromino>,
+    pub(crate) holded_polyomino: Option<Tetromino>,
     pub contain: Container,
 }
 
-#[allow(unused)]
 impl Hold {
-    fn is_holding(&self) -> bool {
-        self.holded_polyomino.is_some()
-    }
-    fn get_holded(&self) -> Option<Tetromino> {
-        self.holded_polyomino
+    fn get_holded(&self) -> &Option<Tetromino> {
+        &self.holded_polyomino
     }
 
-    pub fn switch_with(&mut self, t: Tetromino) -> Option<Tetromino> {
-        if let Some(res) = self.holded_polyomino {
-            self.holded_polyomino = Some(t);
-            Some(res)
+    fn is_holding(&self) -> bool {
+        self.get_holded().is_some()
+    }
+    fn is_none(&self) -> bool {
+        self.get_holded().is_none()
+    }
+
+    fn set_holded(&mut self, t: Option<Tetromino>) -> Result<(), AlreadyHolding> {
+        if self.is_holding() && t.is_some() {
+            Err(AlreadyHolding)
         } else {
-            self.holded_polyomino = Some(t);
+            self.holded_polyomino = t;
+            Ok(())
+        }
+    }
+    
+
+    pub fn take_holded(&mut self) -> Result<Option<Tetromino>, NothingHolded> {
+        if self.is_none() {
+            Err(NothingHolded)
+        } else  {
+            let res = self.holded_polyomino.take();
+            Ok(res)
+        }
+    }
+
+    pub fn switch_with(&mut self, t: Option<Tetromino>) -> Option<Tetromino> {
+        if self.is_holding() {
+            mem::replace(&mut self.holded_polyomino, t)
+        } else {
+            self.set_holded(t);
             None
         }
     }
